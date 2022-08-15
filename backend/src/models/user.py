5e66d92833b760f48842324a3b9f7f6648166c04
Filipe <filipeasm18@gmail.com>
2022-08-14 22:26:46 -0300

    Feat: created User model and schema

---
+++ backend/src/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from ..database.handle_db import Base
+
+
+class UserModel(Base):
+  __tablename__ = "users"
+
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String)
+  email = Column(String, unique=True, index=True)
+  hash_password = Column(String)
+
+  items = relationship("Item", back_populates="owner")
