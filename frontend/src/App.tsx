79dd63383e50d35a26311937d60ad37ef271368e
Filipe <filipeasm18@gmail.com>
2022-08-15 21:37:51 -0300

    Fix: Fixed Axios Cors requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
-import { useEffect, useState } from "react"
-import { api } from "./services/api"
+import { useEffect, useState } from 'react'
+import { api } from './services/api'
 
 export function App() {
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
-    api.get('/api/v1/').then((res) => setMessage(res.data.message))
+    api.get('/api/v1').then(res => setMessage(res.data))
   }, [])
+
+  const handleGetMessage = () => {
+    api.get('/api/v1').then(res => setMessage(res.data))
+  }
 
   return (
     <>
       <div>
         <h1>Rode o servidor para ver a mensagem</h1>
       </div>
+      <div>{message ? message : 'Erro ao fazer o request'}</div>
+
       <div>
-        {message ? message : 'Erro ao fazer o request'}
+        <button onClick={handleGetMessage}>
+          Tentar obter resposta do servidor
+        </button>
       </div>
     </>
   )
