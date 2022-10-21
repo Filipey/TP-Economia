c3e015b5ce203df2f105d3cb024e7b9c453b3d14
Filipe <filipeasm18@gmail.com>
2022-10-21 02:21:14 -0300

    Fix: Order entities by id

--- backend/src/users/service/users.service.ts
+++ backend/src/users/service/users.service.ts
@@ -37,13 +37,13 @@ export class UsersService {
   }
 
   async getUserProducts(cpf: string) {
-    const query = `SELECT p.id, p.nome, p.marca, p.valor, p.estoque FROM produto p, cadastra c WHERE (c.id_produto, cpf_gerente) = (p.id, '${cpf}') ORDER BY p.nome`;
+    const query = `SELECT p.id, p.nome, p.marca, p.valor, p.estoque FROM produto p, cadastra c WHERE (c.id_produto, cpf_gerente) = (p.id, '${cpf}') ORDER BY p.id`;
     const products: Product[] = await this.userRepository.query(query);
     return products;
   }
 
   async getUserMonitoringProducts(cpf: string) {
-    const query = `SELECT p.id, p.nome, p.marca, p.valor, p.estoque, m.data_inicio, m.data_termino, m.expectativa_vendas, m.vendas_realizadas FROM produto p, monitora m WHERE (m.id_produto, cpf_gerente) = (p.id, '${cpf}')`;
+    const query = `SELECT p.id, p.nome, p.marca, p.valor, p.estoque, m.data_inicio, m.data_termino, m.expectativa_vendas, m.vendas_realizadas FROM produto p, monitora m WHERE (m.id_produto, cpf_gerente) = (p.id, '${cpf}') ORDER BY p.id`;
     const products: ProductMonitoringResponseDTO[] =
       await this.userRepository.query(query);
 
